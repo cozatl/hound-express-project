3c909521818e6471d9746fdb28f1488a6cf101f3
jesus cozatl <cozatlram@yahoo.com.mx>
2026-09-15 13:11:23 -0600

    Added project to settings. Improved some configurations. Executed migrations.

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,6 +38,7 @@ LOGIN_URL = "/admin/login/"
 # Application definitions
 INSTALLED_APPS = [
     "pages.apps.PagesConfig",
+    "guides.apps.GuidesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
